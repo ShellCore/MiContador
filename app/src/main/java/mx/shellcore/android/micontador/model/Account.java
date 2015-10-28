4f3d629992cf094a48d054db9fd6c84eaee437e1
Cesar Morales Garduño <shell.one.core@gmail.com>
2015-10-28 17:56:08 -0600

    Se agregaron características a las cuentas.

--- app/src/main/java/mx/shellcore/android/micontador/model/Account.java
+++ app/src/main/java/mx/shellcore/android/micontador/model/Account.java
@@ -1,6 +1,7 @@
 package mx.shellcore.android.micontador.model;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class Account {
     public static final int OTHER = 0;
@@ -11,14 +12,20 @@ public class Account {
     private int type;
     private int currency;
     private BigDecimal beginning_balance;
+    private Date courtDate;
+    private int limitPayDays;
 
     public Account() {
     }
 
-    public Account(int id, String name, int type) {
+    public Account(int id, String name, int type, int currency, BigDecimal beginning_balance, Date courtDate, int limitPayDays) {
         this.id = id;
         this.name = name;
         this.type = type;
+        this.currency = currency;
+        this.beginning_balance = beginning_balance;
+        this.courtDate = courtDate;
+        this.limitPayDays = limitPayDays;
     }
 
     public int getId() {
@@ -43,5 +50,37 @@ public class Account {
 
     public void setType(int type) {
         this.type = type;
+    }
+
+    public int getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(int currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getBeginning_balance() {
+        return beginning_balance;
+    }
+
+    public void setBeginning_balance(BigDecimal beginning_balance) {
+        this.beginning_balance = beginning_balance;
+    }
+
+    public Date getCourtDate() {
+        return courtDate;
+    }
+
+    public void setCourtDate(Date courtDate) {
+        this.courtDate = courtDate;
+    }
+
+    public int getLimitPayDays() {
+        return limitPayDays;
+    }
+
+    public void setLimitPayDays(int limitPayDays) {
+        this.limitPayDays = limitPayDays;
     }
 }
